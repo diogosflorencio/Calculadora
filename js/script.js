d3bb246c649ad3b7d4f15d9a5a60e5eaa6bc467b
Diogo S. Florêncio <33941005+diogosflorencio@users.noreply.github.com>
2024-01-24 11:18:50 -0300

    implementacao de todas as funcionalidades

--- js/script.js
+++ js/script.js
@@ -1,3 +1,34 @@
-function input(param) {
-    document.getElementsByClassName("input")[0].value +=  param;
+const botoes = document.getElementsByClassName("botoes");
+const visor = document.getElementById("input");
+
+for(let i = 0; i < botoes.length; i++){
+    botoes[i].addEventListener("click", event => {
+        if(botoes[i].innerHTML == "AC"){
+            limparVisor();
+        }else if (botoes[i].innerHTML == "DEL") {
+            apagarUltimo();
+        }else if (botoes[i].innerHTML == "%"){
+            porcentagem();
+        }else if (botoes[i].innerHTML == "="){
+            calcular(visor.value);
+        }else {
+            visor.value += botoes[i].innerHTML;
+        }
+    })
+    
+    const limparVisor = () => {
+        visor.value = "  LIMPO!"
+        setTimeout(() => {
+            visor.value = ""
+        },1200)
+    }
+    const apagarUltimo = () => {
+        visor.value = visor.value.slice(0, -1);
+    }
+    const porcentagem = () => {
+        visor.value = visor.value/100;
+    }
+    const calcular = () => {
+        visor.value = eval(visor.value)
+    }
 }
