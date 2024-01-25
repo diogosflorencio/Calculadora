5550f516dd8bb5d479e0dad2763f73ccd2c9a9bd
Diogo S. Florêncio <33941005+diogosflorencio@users.noreply.github.com>
2024-01-25 16:51:37 -0300

    add: script pra mudar de tema

--- js/script.js
+++ js/script.js
@@ -32,3 +32,35 @@ for(let i = 0; i < botoes.length; i++){
         visor.value = eval(visor.value)
     }
 }
+
+// tema 
+
+const body = document.getElementsByClassName("back")[0]
+const calculadora = document.getElementsByClassName("calculadora")[0];
+const botaoTema = document.getElementById("botao-tema");
+
+const mudarTema = () => {
+    if(body.classList.contains("escuro")){
+        calculadora.classList.remove("escuro")
+        calculadora.classList.add("claro")
+        body.classList.remove("escuro")
+        body.classList.add("claro")
+        botaoTema.classList.remove("escuro")
+        botaoTema.classList.add("claro")
+        visor.classList.remove("escuro")
+        visor.classList.add("claro")
+    }else{
+        calculadora.classList.remove("claro")
+        calculadora.classList.add("escuro")
+        body.classList.remove("claro")
+        body.classList.add("escuro")
+        botaoTema.classList.remove("claro")
+        botaoTema.classList.add("escuro")
+        visor.classList.remove("claro")
+        visor.classList.add("escuro")
+    }
+}
+
+botaoTema.addEventListener("click", () => {
+    mudarTema()
+} )
